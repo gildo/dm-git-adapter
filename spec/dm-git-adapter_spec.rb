211c642523d5cefa662fbc08bd44c0bdba05722c
fyskij <fyskij@gmail.com>
2010-12-17 00:53:19 +0100

    added a simple fail spec

--- spec/dm-git-adapter_spec.rb
+++ spec/dm-git-adapter_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/spec_helper'
-require 'dm-core/spec/adapter_shared_spec'
+require 'dm-core/spec/shared/adapter_spec'
 
-describe DataMapper::Adapters::GitAdapter do
+describe 'DataMapper::Adapters::GitAdapter' do
   before :all do
     @adapter = DataMapper.setup(:default, :adapter => 'git',
                                 :hostname => 'localhost',
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,9 @@ require 'rubygems'
 require 'rspec'
 require 'dm-git-adapter'
 require 'dm-core'
+
+require 'dm-core/spec/lib/pending_helpers'
+
+RSpec.configure do |config|
+    config.include(DataMapper::Spec::PendingHelpers)
+end
