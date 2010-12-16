c459009f1ab6ca45b83942a50a47167e85ac02ba
fyskij <fyskij@gmail.com>
2010-12-16 22:24:26 +0100

    simple setup spec

---
+++ spec/dm-git-adapter_spec.rb
@@ -0,0 +1,12 @@
+require File.dirname(__FILE__) + '/spec_helper'
+require 'dm-core/spec/adapter_shared_spec'
+
+describe DataMapper::Adapters::GitAdapter do
+  before :all do
+    @adapter = DataMapper.setup(:default, :adapter => 'git',
+                                :hostname => 'localhost',
+                                :database => 'dm-git-adapter')
+    end
+
+    it_should_behave_like 'An Adapter'
+end
